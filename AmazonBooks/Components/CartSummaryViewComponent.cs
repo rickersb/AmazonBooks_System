e090bf8b59e50fbf91e5b3246d0d4da04bb9ad86
rickersb <80800979+rickersb@users.noreply.github.com>
2022-03-02 17:22:51 -0700

    all done except cart icon only partial. code is in place

---
+++ AmazonBooks/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AmazonBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AmazonBooks.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+        public CartSummaryViewComponent(Basket cartService)
+        {
+            basket = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
