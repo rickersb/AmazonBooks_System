0813fd13ad57be932c4b8bc5d65b682637bd00b7
rickersb <80800979+rickersb@users.noreply.github.com>
2022-03-07 15:55:23 -0700

    commit

--- AmazonBooks/Models/IBookstoreRepository.cs
+++ AmazonBooks/Models/IBookstoreRepository.cs
@@ -6,6 +6,10 @@ namespace AmazonBooks.Models
     public interface IBookstoreRepository
     {
         IQueryable<Book> Books { get; }
-       
+
+        public void SaveBook(Book b);
+        public void CreateBook(Book b);
+        public void DeleteBook(Book b);
+
     }
 }
